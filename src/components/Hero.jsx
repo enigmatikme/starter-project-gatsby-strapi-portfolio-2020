c1f0bd9367f38a29d4bf778e736a14975badb0fe
Karin Hsu <khsu0720@gmail.com>
2020-08-31 16:53:52 +0800

    hero navbar added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import SocialLinks from "../constants/socialLinks"
+
+const query = graphql`
+{
+  file(relativePath: {eq: "hero-img.png"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+        # src
+      }
+    }
+  }
+}
+`
+
+const Hero = () => {
+  const {
+    file: { 
+      childImageSharp: {
+        fluid
+      }
+    }
+  } = useStaticQuery(query);
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div className="underline"></div>
+          <h1>i'm Karin</h1>
+          <h4>full stack engineer</h4>
+          <Link to='/contact' className="btn">
+            contact me
+          </Link>
+          <SocialLinks />
+        </article>
+        <Image fluid={fluid} className="hero-img"/>
+      </div>
+    </header>
+    )
+}
+
+export default Hero
